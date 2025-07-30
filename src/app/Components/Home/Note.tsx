de17ad12d0c0a0162546edbba67f9a394d5af59c
Roshan SaiHam Nafiz <118830871+iamnafiz10@users.noreply.github.com>
2025-07-30 14:33:30 +0600

    update again

--- src/app/Components/Home/Note.tsx
+++ src/app/Components/Home/Note.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import Link from "next/link";
-import heroImage from "../../../../public/assets/images/home-hero.png";
-
-function Hero() {
-    return (
-        <>
-            <section id="banner-section" className="bg-cover bg-no-repeat bg-center h-[650px] py-[10rem]"
-                     style={{backgroundImage: `url(${heroImage.src})`}}
-            >
-                <div className="container">
-                    <h1 className="text-2xl md:text-[38px] leading-6 md:leading-10 text-[#263F5C] font-bold md:font-[600] mb-4">
-                        Des livres...<br/>
-                        <span className="text-[#3A5F8A]">
-                            pour faire avancer
-                        </span>
-                        <span className="text-primary"> l{`’`}humanité</span>
-                    </h1>
-                    <p>
-                        Nous concevons des ouvrages éducatifs qui éclairent, inspirent et transforment.
-                    </p>
-                    <div className="mt-8">
-                        <Link href='#'
-                              className="bg-primary border border-primary px-8 py-3 text-white font-medium rounded-md hover:bg-transparent hover:text-primary transition">
-                            Découvrir nos collections
-                        </Link>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
-}
-
-export default Hero;
+// import React from 'react';
+// import Link from "next/link";
+// import heroImage from "../../../../public/assets/images/home-hero.png";
+//
+// function Hero() {
+//     return (
+//         <>
+//             <section id="banner-section" className="bg-cover bg-no-repeat bg-center h-[650px] py-[10rem]"
+//                      style={{backgroundImage: `url(${heroImage.src})`}}
+//             >
+//                 <div className="container">
+//                     <h1 className="text-2xl md:text-[38px] leading-6 md:leading-10 text-[#263F5C] font-bold md:font-[600] mb-4">
+//                         Des livres...<br/>
+//                         <span className="text-[#3A5F8A]">
+//                             pour faire avancer
+//                         </span>
+//                         <span className="text-primary"> l{`’`}humanité</span>
+//                     </h1>
+//                     <p>
+//                         Nous concevons des ouvrages éducatifs qui éclairent, inspirent et transforment.
+//                     </p>
+//                     <div className="mt-8">
+//                         <Link href='#'
+//                               className="bg-primary border border-primary px-8 py-3 text-white font-medium rounded-md hover:bg-transparent hover:text-primary transition">
+//                             Découvrir nos collections
+//                         </Link>
+//                     </div>
+//                 </div>
+//             </section>
+//         </>
+//     );
+// }
+//
+// export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Slider from "@/app/Components/Home/Slider";
-import Here from "@/app/Components/Home/Hero";
 import Education from "@/app/Components/Home/Education";
 import En from "@/app/Components/Home/En";
+import Hero from "@/app/Components/Home/Hero";
 
 export default function Home() {
     return (
         <>
-            <Here/>
+            <Hero/>
             <Slider/>
             <Education/>
             <En/>
