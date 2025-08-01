a18c5b008837c3296def4a6b061d09d7f754e7ea
Roshan SaiHam Nafiz <118830871+iamnafiz10@users.noreply.github.com>
2025-08-01 20:39:55 +0600

    Upload error solve

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import Hero from "@/app/Components/Profile/Hero";
-// import Biographie from "@/app/Components/Profile/Biographie";
-//
-// function Page() {
-//     return (
-//         <>
-//             <Hero/>
-//             <Biographie/>
-//         </>
-//     );
-// }
-//
-// export default Page;
+import React from 'react';
+import Hero from "@/app/Components/Profile/Hero";
+import Biographie from "@/app/Components/Profile/Biographie";
+
+function Page() {
+    return (
+        <>
+            {/*<Hero/>*/}
+            {/*<Biographie/>*/}
+        </>
+    );
+}
+
+export default Page;
